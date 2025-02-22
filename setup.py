c7736a40d3697c26516f49df214dbe86a68845ff
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-22 13:49:06 +0900

    "pythonmacess as using mylibpkgset set"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pythonmacess',
-    version='0.0.1',
+    version='0.0.2',
     description='`python -m` acesss',
     author='du7ec',
     author_email='dutec6834@gmail.com',
